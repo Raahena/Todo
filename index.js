9c54af6e7061753d8c135996052cf907e4fea66f
Raahena Showkath <raahenashowkath@gmail.com>
2024-01-21 23:42:49 +0530

    inital

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app=express();
+const port=3000;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+let todos= [];
+function getCurrentDate() {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const today = new Date().toLocaleDateString('en-US', options);
+  return today;
+}
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {todos, getCurrentDate});
+  });
+
+app.post("/submit",(req,res)=>{
+    const newTodo = req.body["submitData"];
+    todos.push(newTodo);
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
